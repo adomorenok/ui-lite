09d98b593b481fcf71d87047953f00a549fd72f8
Vsevolod Astanov <vsevolod.astanov@gmail.com>
2017-04-26 13:57:47 +0300

    add document ready event

--- src/js/services/menu-service.js
+++ src/js/services/menu-service.js
@@ -2,69 +2,71 @@
 
     'use strict';
 
-    var submenu = document.getElementsByClassName('ui-submenu')[0];
-    var container = document.getElementsByClassName('ui-container')[0];
-    var sidebar = document.getElementsByClassName('ui-sidebar')[0];
-    var panel = document.getElementsByClassName('ui-panel')[0];
+    document.onReady(function() {
+        var submenu = document.getElementsByClassName('ui-submenu')[0];
+        var container = document.getElementsByClassName('ui-container')[0];
+        var sidebar = document.getElementsByClassName('ui-sidebar')[0];
+        var panel = document.getElementsByClassName('ui-panel')[0];
 
-    function openSubMenu() {
-        submenu.classList.add('ui-submenu-active');
-        if (submenu.classList.contains('ui-submenu-left')) {
-            container.classList.add('ui-container-has-left-open-submenu');
-        } else {
-            container.classList.add('ui-container-has-right-open-submenu');
-        }
-        if (sidebar) {
-            sidebar.classList.add('ui-sidebar-hidden');
-        }
+        function openSubMenu() {
+            submenu.classList.add('ui-submenu-active');
+            if (submenu.classList.contains('ui-submenu-left')) {
+                container.classList.add('ui-container-has-left-open-submenu');
+            } else {
+                container.classList.add('ui-container-has-right-open-submenu');
+            }
+            if (sidebar) {
+                sidebar.classList.add('ui-sidebar-hidden');
+            }
 
-        addCloseEventByClickOnSubmenu();
-    }
-
-    function closeSubMenu() {
-        if (sidebar) {
-            sidebar.classList.remove('ui-sidebar-hidden');
-        }
-        submenu.classList.remove('ui-submenu-active');
-        submenu.classList.add('ui-submenu-hidden');
-
-        if (submenu.classList.contains('ui-submenu-left')) {
-            container.classList.remove('ui-container-has-left-open-submenu');
-        } else {
-            container.classList.remove('ui-container-has-right-open-submenu');
+            addCloseEventByClickOnSubmenu();
         }
 
-        setTimeout(function () {
-            submenu.classList.remove('ui-submenu-hidden');
-        }, 150);
+        function closeSubMenu() {
+            if (sidebar) {
+                sidebar.classList.remove('ui-sidebar-hidden');
+            }
+            submenu.classList.remove('ui-submenu-active');
+            submenu.classList.add('ui-submenu-hidden');
 
-        document.documentElement.removeEventListener('click', closeSubMenu, true);
-    }
+            if (submenu.classList.contains('ui-submenu-left')) {
+                container.classList.remove('ui-container-has-left-open-submenu');
+            } else {
+                container.classList.remove('ui-container-has-right-open-submenu');
+            }
 
-    function closePanel(e) {
-        panel.classList.remove('ui-panel-active');
-        document.documentElement.removeEventListener('click', closePanel, true);
-    }
+            setTimeout(function () {
+                submenu.classList.remove('ui-submenu-hidden');
+            }, 150);
 
-    function addCloseEventByClickOnSubmenu() {
-        document.documentElement.addEventListener('click', closeSubMenu, true);
-    }
+            document.documentElement.removeEventListener('click', closeSubMenu, true);
+        }
 
-    function addCloseEventByClickOnPanel() {
-        panel.classList.add('ui-panel-active');
-        document.documentElement.addEventListener('click', closePanel, true);
-    }
+        function closePanel(e) {
+            panel.classList.remove('ui-panel-active');
+            document.documentElement.removeEventListener('click', closePanel, true);
+        }
 
-    var menuService = (function () {
-        return {
-            closeSubMenu: closeSubMenu,
-            openSubMenu: openSubMenu,
-            addCloseEventByClickOnSubmenu: addCloseEventByClickOnSubmenu,
-            addCloseEventByClickOnPanel: addCloseEventByClickOnPanel
-        };
-    })();
+        function addCloseEventByClickOnSubmenu() {
+            document.documentElement.addEventListener('click', closeSubMenu, true);
+        }
 
-    window.menuService = menuService;
+        function addCloseEventByClickOnPanel() {
+            panel.classList.add('ui-panel-active');
+            document.documentElement.addEventListener('click', closePanel, true);
+        }
+
+        var menuService = (function () {
+            return {
+                closeSubMenu: closeSubMenu,
+                openSubMenu: openSubMenu,
+                addCloseEventByClickOnSubmenu: addCloseEventByClickOnSubmenu,
+                addCloseEventByClickOnPanel: addCloseEventByClickOnPanel
+            };
+        })();
+
+        window.menuService = menuService;
+    });
 })();
 
 		
