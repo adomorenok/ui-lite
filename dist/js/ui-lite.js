73a4fa11c17c773bca995449818ea93555a33872
aliaksandr.belavus <aliaksandr.belavus@gmail.com>
2018-02-27 12:48:07 +0300

    Issue VW-1608 Incorrect display of the mobile version of the site with a width of more than 800 pixels. fixed request tabs
    fixed

--- dist/js/ui-lite.js
+++ dist/js/ui-lite.js
@@ -530,6 +530,20 @@
         }
     }
 
+    function closeSubMenuExcludingPinnedMode() {
+        var submenuList = document.documentElement.getElementsByClassName('ui-submenu');
+        for (var i = 0; i < submenuList.length; i++) {
+            if (!submenuList[i].classList.contains('ui-submenu__pinned-mode')) {
+                submenuList[i].classList.remove('ui-submenu-active');
+            }
+        }
+
+        var sidebar = document.documentElement.getElementsByClassName('ui-sidebar-small');
+        for (var s = 0; s < sidebar.length; s++) {
+            sidebar[s].classList.remove('ui-sidebar-small');
+        }
+    }
+
     function closePanel(e) {
         removeClassToElements(document.documentElement.getElementsByClassName('ui-header'), 'active');
         removeClassToElements(document.documentElement.getElementsByClassName('ui-panel'), 'ui-panel-active');
@@ -619,7 +633,8 @@
             setMenuScroll: setMenuScroll,
             removePadding: removePadding,
             addCloseEventForSubmenu: addCloseEventForSubmenu,
-            addCloseEventByClickOnPanel: addCloseEventByClickOnPanel
+            addCloseEventByClickOnPanel: addCloseEventByClickOnPanel,
+            closeSubMenuExcludingPinnedMode: closeSubMenuExcludingPinnedMode
         };
     })();
 })();
@@ -822,9 +837,9 @@
     };
 
     UISidebarBtn.prototype.showSidebar = function(e) {
-        ui.menuService.closeSubMenu();
+        ui.menuService.closeSubMenuExcludingPinnedMode();
         var sidebar = e.currentTarget.offsetParent;
-        if(!sidebar.classList.contains('ui-sidebar-active')) {
+        if(!sidebar.classList.contains('ui-sidebar-active') && !existActivePinnedModeSubmenu()) {
             sidebar.classList.add('ui-sidebar-active');
 
             sidebar.addEventListener('mouseleave', hideSidebar, false);
@@ -833,6 +848,19 @@
         function hideSidebar(e) {
             e.currentTarget.classList.remove('ui-sidebar-active');
             sidebar.removeEventListener('mouseleave', hideSidebar, false);
+        }
+
+        function existActivePinnedModeSubmenu() {
+            var exist = false;
+            var submenuList = document.documentElement.getElementsByClassName('ui-submenu');
+            for (var i = 0; i < submenuList.length; i++) {
+                if (submenuList[i].classList.contains('ui-submenu-active') &&
+                    submenuList[i].classList.contains('ui-submenu__pinned-mode')) {
+                   exist = true;
+                   break;
+                }
+            }
+            return exist;
         }
     };
 
